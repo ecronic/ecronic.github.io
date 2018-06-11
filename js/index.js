939bb79c73a8b3324d9036172219b949bab6419b
ecronic <907939520@qq.com>
2018-06-11 23:24:41 +0800

    Site updated: 2018-06-11 23:24:41

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 window.onload = function(){
+    (function () {
+        var winHeight = 0;
+        if (window.innerHeight){
+            winHeight = window.innerHeight;
+        }
+        else if (document.body && document.body.clientHeight){
+            winHeight = document.body.clientHeight;
+        }
+        else if(document.documentElement && document.documentElement.clientHeight){
+            winHeight = document.documentElement.clientHeight;
+        }
+        var contentHeight = winHeight - 60;
+        document.getElementById("content").setAttribute('style', 'min-height: '+contentHeight+'px !important');
+    } ());
     const menu = document.querySelector('.menu')
 
     menu.addEventListener('click', toogleList)
@@ -14,5 +28,4 @@ window.onload = function(){
     const highloghtBlocks = document.querySelectorAll('figure.highlight')
 
     highloghtBlocks.length && highloghtBlocks.forEach( i => hljs.highlightBlock(i) )
-
 }
