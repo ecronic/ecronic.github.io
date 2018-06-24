6a8e863487d812e28ada04eb909a8eb7b36d0dd5
ecronic <907939520@qq.com>
2018-06-24 23:53:14 +0800

    Site updated: 2018-06-24 23:53:14

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
 window.onload = function(){
     const menu = document.querySelector('.menu')
-
-    this.console.log(menu)
     menu.addEventListener('click', toogleList)
 
     function toogleList(){
-
-        console.log("ddddd")
         const menuList = document.querySelector('.menu_list')
-
         menuList.classList.toggle('hide')
         menuList.classList.toggle('show')
     }
 
     const highloghtBlocks = document.querySelectorAll('figure.highlight')
-
     highloghtBlocks.length && highloghtBlocks.forEach( i => hljs.highlightBlock(i) )
 }
